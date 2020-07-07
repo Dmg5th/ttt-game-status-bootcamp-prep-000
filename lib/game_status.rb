7b289cade766a80a21980ae8bc6d76e65073a6a3
Daniel Gould <dmgould310@gmail.com>
2020-07-07 14:06:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,13 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], #last row
+  [0,3,6], #first row down
+  [1,4,7], #second row down
+  [2,5,8], #third row down
+  [0,4,8], #first diagonal
+  [2,4,6], #second diagonal
+ ]
