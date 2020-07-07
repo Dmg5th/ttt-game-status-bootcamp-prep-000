4354eb97f1d9e3ac4da513fe56e527c38b0f90e5
Daniel Gould <dmgould310@gmail.com>
2020-07-07 14:30:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,15 +15,20 @@ WIN_COMBINATIONS = [
   [2,4,6], #second diagonal
  ]
  
- def won?(board)
-   WIN_COMBINATIONS.each do |array|
-     array.each do |element|
-       element.each |position|
-       if position = board
-         return position
-       else 
-         nil
-        end
-      end
+WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
     end
-  end
+  }
+  return false
+end
